67050ada984a8d4bbb7c7239a3bcdef297e4069a
shital bramhe <shitalbramhe@gmail.com>
2021-11-24 14:49:14 +0530

    [shital] ADD: Binary search tree created

---
+++ Binarysearchtree/Binarysearchtree/Binary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binarysearchtree
+{
+    public class Binary<T> where T : IComparable
+    {
+        T nodedata;
+        public Binary<T> lefttree { get; set; }
+        public Binary<T> righttree { get; set; }
+        bool result = false;
+
+        public Binary(T nodedata)
+        {
+            this.nodedata = nodedata;
+            this.nodedata = nodedata;
+            this.lefttree = null;
+            this.righttree = null;
+        }
+ 
+        public void Insert(T item)
+        {
+            T currentnodevalue = this.nodedata;
+            if ((currentnodevalue.CompareTo(item)) > 0)
+            {
+                if (this.lefttree == null)
+                    this.lefttree = new Binary<T>(item);
+                else
+                    this.lefttree.Insert(item);
+            }
+            else
+            {
+                if (this.righttree == null)
+                    this.righttree = new Binary<T>(item);
+                else
+                    this.righttree.Insert(item);
+            }
+        }
+          public void Display()
+        {
+            if (this.lefttree != null)
+            {
+                this.lefttree.Display();
+            }
+            Console.WriteLine(this.nodedata.ToString());
+            if (this.righttree != null)
+            {
+                this.righttree.Display();
+            }
+        }
+    }
+}
+
---
+++ Binarysearchtree/Binarysearchtree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binarysearchtree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Binary<int> bst = new Binary<int>(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            Console.WriteLine("Binary Search Tree ");
+            bst.Display();
+            Console.ReadLine();
+        }
+    }
+}
